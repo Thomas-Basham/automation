ad10c0213997253901cabd4226c0cc6a78fb6d1f
Thomas Basham <Bashamtg@gmail.com>
2022-05-12 14:08:10 -0700

    emails matching

--- automation_/validate_creds.py
+++ automation_/validate_creds.py
@@ -3,16 +3,20 @@ string = ''
 with open("assets/existing-contacts.txt") as f:
     for line in f:
         string += line
-        # print(line)
-# with open("assets/existing-contacts.txt") as f:
-#     text_from_file = f.read()
-#     for line in f:
-#         print(line)
 
-pattern = r"(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+# https://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script
+# https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
 
-phone_nums = re.findall(pattern, string)
-# print(string)
+phone_pattern = r"(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+phone_nums = re.findall(phone_pattern, string)
+
+emails = re.findall(email_pattern, string)
+
+
 print(phone_nums)
+print(emails)
+
 
 assert len(phone_nums) == 20, f"Found {len(phone_nums)} social security numbers"
